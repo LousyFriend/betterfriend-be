5f256ea4bccdb79b3d380f631d82a315da12bc5b
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-18 16:33:46 -0700

    Add tables, seed data, get Contacts

---
+++ data/contacts.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'Dan',
+    job_title: 'Software Engineer',
+    image_url: null,
+    interests: null,
+    contact_category: 'personal'
+  },
+  {
+    name: 'Michael',
+    job_title: 'Software Engineer',
+    image_url: null,
+    interests: 'cat wrangling',
+    contact_category: 'personal'
+  },
+  {
+    name: 'Zack',
+    job_title: 'Software Engineer',
+    image_url: null,
+    interests: 'cleaning up cheerios off the floor',
+    contact_category: 'personal'
+  }
+];
