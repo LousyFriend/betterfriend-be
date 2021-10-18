6ddd2485948f9d70bce61f6f232abeaef09cdf24
Michael Rider <mikepdxrider@gmail.com>
2021-10-18 14:54:21 -0700

    establishes routes

--- data/animals.js
+++ data/animals.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     name: 'bessie',
-    cool_factor: 3,
+    cool_factor: 3
   },
   {
     name: 'jumpy',
-    cool_factor: 4,
+    cool_factor: 4
   },
   {
     name: 'spot',
-    cool_factor: 10,
+    cool_factor: 10
   }
 ];
