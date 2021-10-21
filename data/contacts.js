ba29593cbac8771f8b947790ce0e1f365bda8532
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-21 15:58:46 -0700

    Add calendar routes

--- data/contacts.js
+++ data/contacts.js
@@ -4,20 +4,26 @@ module.exports = [
     job_title: 'Software Engineer',
     image_url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jxDMB8xZsly6tzU5JtNo2AHaHa%26pid%3DApi&f=1',
     interests: 'final jeopardy, monster trucks with unicorns painted on the sides',
-    contact_category: 'personal'
+    contact_category: 'personal',
+    next_date: null,
+    event_id: null
   },
   {
     name: 'Michael',
     job_title: 'Software Engineer',
     image_url: null,
     interests: 'cat wrangling',
-    contact_category: 'personal'
+    contact_category: 'personal',
+    next_date: null,
+    event_id: null
   },
   {
     name: 'Zack',
     job_title: 'Software Engineer',
     image_url: null,
     interests: 'cleaning up cheerios off the floor',
-    contact_category: 'personal'
+    contact_category: 'personal',
+    next_date: null,
+    event_id: null
   }
 ];
