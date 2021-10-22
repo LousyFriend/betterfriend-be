b4c236762a887c6f18a3e467c4769723ef97d044
Michael Rider <mikepdxrider@gmail.com>
2021-10-21 17:02:12 -0700

    resolves weird table errors

--- data/create-tables.js
+++ data/create-tables.js
@@ -25,8 +25,8 @@ async function run() {
                     interests VARCHAR(512),
                     contact_category VARCHAR(512) NOT NULL,
                     next_date VARCHAR(512),
-                    event_id VARCHAR(512) 
-                    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+                    event_id VARCHAR(512), 
+                    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
                 );
                 CREATE TABLE social_media (
                   contact_id INTEGER NOT NULL REFERENCES contacts(id) ON DELETE CASCADE,
