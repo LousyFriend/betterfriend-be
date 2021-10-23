3209ea90f6a8115bb587c75b447123d18d61ac5c
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-23 13:25:45 -0700

    Working calendar tests

--- __tests__/calendar.test.js
+++ __tests__/calendar.test.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-// const { execSync } = require('child_process');
+const { execSync } = require('child_process');
 
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
@@ -11,36 +11,81 @@ describe('app routes', () => {
     let token;
   
     beforeAll(async () => {
-      // execSync('npm run setup-db');
+      execSync('npm run setup-db');
   
       await client.connect();
       const signInData = await fakeRequest(app)
-        .post('/auth/signin')
+        .post('/auth/signup')
         .send({
-          email: 'john@arbuckle.com',
+          email: 'jon@user.com',
           password: '1234'
         });
       
       token = signInData.body.token; // eslint-disable-line
-    }, 10000);
+
+      await fakeRequest(app)
+        .post('/api/contacts/')
+        .send({ name: 'John',
+          job_title: 'SWD / SWE',
+          image_url: '',
+          interests: '',
+          contact_category: 'personal',
+          linked_in: '',
+          facebook: '',
+          gmail: '',
+          phone: '',
+          twitter: '',
+          github: '',
+          personal_site: ''
+        })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/);
+     
+    }, 30000);
   
     afterAll(done => {
       return client.end(done);
     });
 
-    test('fake test', async() => {
+    test('contact calendar put request', async() => {
 
-      const expectation = {
-        next_date: '20211023',
-        event_id: 'testing'
-      };
+      const expectation = [{
+        'id': expect.any(Number),
+        'name': expect.any(String),
+        'job_title': expect.any(String),
+        'image_url': expect.any(String),
+        'interests': expect.any(String),
+        'contact_category': expect.any(String),
+        'next_date': '20211023',
+        'event_id': 'event123',
+        'user_id': expect.any(Number),
+      }];
 
       const data = await fakeRequest(app)
-        .get('/api/contact/calendar/4');
-        // .expect('Content-Type', /json/)
-        // .expect(200);
+        .put('/api/contact/calendar/4')
+        .send({ next_date: '20211023',
+          event_id: 'event123' })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-      expect(data).toEqual(expectation);
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('fake test', async() => {
+
+      const expectation = [{
+        next_date: '20211023',
+        event_id: 'event123'
+      }];
+
+      const data = await fakeRequest(app)
+        .get('/api/contact/calendar/4')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
     });
   });
 });
--- data/contacts.js
+++ data/contacts.js
@@ -5,8 +5,8 @@ module.exports = [
     image_url: 'https://scontent.fhio2-1.fna.fbcdn.net/v/t1.6435-9/90049738_10107806145139532_7381357969163681792_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=xI4KihkZcbIAX8Y2wed&_nc_ht=scontent.fhio2-1.fna&oh=af3239af20cdf731a15f769fe9d02c2e&oe=619A4AC2',
     interests: 'Jogging, Hiking, Traveling',
     contact_category: 'personal',
-    next_date: '20211023',
-    event_id: 'testing'
+    next_date: null,
+    event_id: null
   },
   {
     name: 'Michael',
